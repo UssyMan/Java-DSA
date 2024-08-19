fe352c310c47db7d92ef6a1937d043d44433500f
Uthman Yahaya Baba <abustuff@gmail.com>
2024-08-19 21:52:36 +0100

    Implemented append and removing last node in a LL

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -1,2 +1,93 @@
-package datastructures.linkedlist;public class LinkedList {
+package datastructures.linkedlist;
+
+
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+
+    static class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+    public Node removeLast(){
+        if (length==0) return null;
+        Node temp = head;
+        Node pre= temp;
+
+        while (temp.next!=null){
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+
+        if (length==0){
+            head = null;
+            tail = null;
+        }
+
+        return temp;
+    }
 }
--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -1,5 +1,15 @@
+package datastructures.linkedlist;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
     }
 }
